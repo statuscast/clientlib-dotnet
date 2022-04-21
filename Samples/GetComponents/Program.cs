49c7ab81861209afc5a94c0e3a1a8dfc6d89b4ab
scottkocur-sc <scott@statuscast.com>
2022-04-21 16:27:34 -0400

    get components update

---
+++ Samples/GetComponents/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+
+var statuspage = "https://<STATUSPAGE>";
+var username = "<USERNAME>";
+var password = "<PASSWORD>";
+
+var httpClient = new HttpClient();
+var apiClient = new StatusCast.Client(httpClient)
+{
+    BaseUrl = statuspage
+};
+
+var auth = apiClient.Authenticate(new StatusCast.AuthenticationRequest
+{
+    UserName = username,
+    Password = password
+});
+
+httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", auth.Token);
+
+var components = apiClient.ComponentsAsync();
+
+Console.WriteLine($"{components.Result.Count} components found");
